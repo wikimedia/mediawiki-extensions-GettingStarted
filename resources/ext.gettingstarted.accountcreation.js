77d2035d5efe06d7f63914f2b361c16bbc971841
S Page <spage@wikimedia.org>
2012-12-19 02:28:01 -0800

    Copy title attr to new #back-to-referrer button
    The old '#mw-return-to button put the article title in the title
    attribute, and event logging needs it.
    Also add 'use strict' for jshint, and avoid "Too many var statements."
    
    Change-Id: I9c2d42223c75462fcb9552bde5e2061f52e68d63

--- resources/ext.gettingstarted.accountcreation.js
+++ resources/ext.gettingstarted.accountcreation.js
@@ -1,26 +1,33 @@
 ( function ( $, mw ) {
+	'use strict';
 	$( function () {
-		var title = mw.msg( 'gettingstarted-welcomesiteuser', mw.config.get( 'wgSiteName' ), mw.config.get( 'wgUserName' ) );
+		var title = mw.msg( 'gettingstarted-welcomesiteuser', mw.config.get( 'wgSiteName' ), mw.config.get( 'wgUserName' ) ),
+			$returnTo = $( '#mw-returnto' ),
+			$returnToA = $( $returnTo ).find( 'a' ),
+			returnToUrl = $returnToA.attr( 'href' ),
+			returnToTitle = $returnToA.attr( 'title' ),
+			$returnToAgora,
+			state,
+			url;
 
 		document.title = title;
 		$( '#firstHeading span' ).text( title );
 
 		// Remove original return to button, then add Agora one in header.
-		var $returnTo = $( '#mw-returnto' );
-		var returnToUrl = $( 'a', $returnTo ).attr( 'href' );
 		$returnTo.remove();
 
-		var $returnToAgora = $( '<a />', {
+		$returnToAgora = $( '<a />', {
 			id: 'back-to-referrer',
 			href: returnToUrl,
+			title: returnToTitle,
 			'class': 'blue mw-ui-button',
 			text: '← ' + mw.msg('gettingstarted-return')
 		} );
 
 		$( '#onboarding-header p' ).append( $returnToAgora );
 
-		var state = {}; // Currently unused
-		var url = mw.util.wikiGetlink( 'Special:GettingStarted' );
+		state = {}; // Currently unused
+		url = mw.util.wikiGetlink( 'Special:GettingStarted' );
 		if ( history.replaceState ) {
 			history.replaceState( state, title, url );
 		}
