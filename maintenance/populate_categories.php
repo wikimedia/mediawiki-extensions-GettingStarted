abd312b19343e781df2782e372e71a83f7dbc4e7
Ori Livneh <ori@wikimedia.org>
2014-01-08 21:27:40 -0500

    Import populate script from Gist
    Original code by Ori Livneh, at https://gist.github.com/atdt/5159480
    
    Co-Author: Matthew Flaschen <mflaschen@wikimedia.org>
    Bug: 46299
    Change-Id: Iae074bb8fd83f2e923ccd84b3b5f80d51cd836e9

---
+++ maintenance/populate_categories.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace GettingStarted;
+
+use Category;
+
+/**
+ * Populate GettingStarted Redis sets from categories.  The members of the sets are
+ * article IDs.  This is run when GettingStarted is first installed.  The hooks should
+ * keep the sets up to date after that.
+ */
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+
+require_once "$IP/maintenance/Maintenance.php";
+
+class PopulateCategories extends \Maintenance {
+	private $mClient;
+
+	public function __construct() {
+		$this->mDescription = 'Populates GettingStarted Redis sets from categories';
+	}
+
+	private function populateCategory( Category $category ) {
+		$key = RedisCategorySync::makeCategoryKey( $category );
+		echo "Key: $key\n";
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+			array( 'page', 'categorylinks' ),
+			array( 'page_id' ),
+			array(
+				'cl_from = page_id',
+				'cl_to' => $category->getName(),
+				'page_is_redirect' => 0,
+				'page_namespace' => NS_MAIN,
+			),
+			__FUNCTION__
+		);
+
+		$pages = array();
+		foreach( $res as $row ) {
+			$pages[] = $row->page_id;
+		}
+		if ( !count( $pages ) ) {
+			return 0;
+		}
+
+
+		$redis = $this->mClient->multi( \Redis::PIPELINE );
+
+		$batches = array_chunk( $pages, 100 );
+		foreach( $batches as $batch ) {
+			array_unshift( $batch, $key );
+			call_user_func_array( array( $redis, 'sAdd' ), $batch );
+		}
+		return $redis->exec() ? count( $pages ) : 0;
+	}
+
+	public function execute() {
+		$this->mClient = RedisCategorySync::getClient();
+		if ( !$this->mClient ) {
+			$this->error( 'Failed to get Redis connection. Exiting.', 1 );
+		}
+
+		foreach( RedisCategorySync::getCategories() as $catName ) {
+			echo "Populating category '${catName}' ...\n";
+			$cat = Category::newFromName( $catName );
+			$count = $this->populateCategory( $cat );
+			echo "Updated / refreshed $count pages in category.\n\n";
+		}
+	}
+}
+
+$maintClass = 'GettingStarted\PopulateCategories';
+require_once( RUN_MAINTENANCE_IF_MAIN );
