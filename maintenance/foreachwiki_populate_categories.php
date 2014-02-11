c7b1da6662bf47277f50ea2f6b3f25dc15850606
Matthew Flaschen <mflaschen@wikimedia.org>
2014-02-11 12:10:14 -0500

    Add foreachwiki populate_category script.
    Not tested yet.
    
    Change-Id: I5655f6115612317da1aaa7551d19f15eaec5ef9e

---
+++ maintenance/foreachwiki_populate_categories.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( $wmgUseGettingStarted && ( count( $wgGettingStartedCategoriesForTaskTypes ) > 0 ) ) {
+	$path = __DIR__ . '/populate_categories.php';
+
+	// Don't put this in a function, or Maintenance::shouldExecute will return false
+	require_once "$path";
+}
