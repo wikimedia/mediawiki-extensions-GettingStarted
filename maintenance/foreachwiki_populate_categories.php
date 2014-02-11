9dec623dedbc27f057bd7e34ef9be9869e4fa2bd
Matthew Flaschen <mflaschen@wikimedia.org>
2014-02-11 14:12:37 -0500

    Remove foreachwiki_populate_categories.php
    This didn't work.  Instead, we just ran:
    
    sudo -u apache foreachwikiindblist /a/common/gettingstarted-with-category-suggestions.dblist GettingStarted/maintenance/populate_categories.php
    
    Change-Id: I33c22234d3c19683f6c66aedf585854623e7ce59

--- maintenance/foreachwiki_populate_categories.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ( count( $wgGettingStartedCategoriesForTaskTypes ) > 0 ) {
-	$path = __DIR__ . '/populate_categories.php';
-
-	// Don't put this in a function, or Maintenance::shouldExecute will return false
-	require_once "$path";
-}
