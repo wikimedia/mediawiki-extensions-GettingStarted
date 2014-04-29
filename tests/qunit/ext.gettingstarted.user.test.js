wikimedia/mediawiki-extensions-GettingStarted

commit 533c7f3b5821f6637f349dd0fd8ecd6a0de660d0

Generate a token and use it to bucket users
* Create the mw.gettingStarted.user object, which can be used to get a
  user's token and the bucket that they are in (getToken and getBucket
  respectively)
* Make mw.gettingStarted.user.getToken transparently persist the token
  to a cookie for 90 days

Change-Id: I4c35a2bece411e7f0b10ce9a22f2aa592b77c388

GettingStarted.php
@@ -245,6 +245,14 @@ $wgResourceModules[ 'ext.gettingstarted.return' ] = array(
 	'position' => 'top',
 ) + $gettingStartedModuleInfo;
 
+$wgResourceModules[ 'ext.gettingstarted.user' ] = array(
+	'scripts' => 'ext.gettingstarted.user.js',
+	'dependencies' => array(
+		'mediawiki.user',
+		'jquery.cookie',
+	),
+) + $gettingStartedModuleInfo;
+
 $wgDefaultUserOptions[ GettingStarted\Hooks::INTRO_OPTION ] = true;
 
 $wgHooks[ 'BeforePageDisplay' ][] = 'GettingStarted\Hooks::onBeforePageDisplay';
@@ -259,3 +267,4 @@ $wgHooks[ 'GetPreferences' ][] = 'GettingStarted\Hooks::onGetPreferences';
 $wgHooks[ 'UserLogoutComplete'][] = 'GettingStarted\Hooks::onUserLogoutComplete';
 // Extension:CentralAuth's hook
 $wgHooks[ 'CentralAuthPostLoginRedirect' ][] = 'GettingStarted\Hooks::onCentralAuthPostLoginRedirect';
+$wgHooks[ 'ResourceLoaderTestModules' ][] = 'GettingStarted\Hooks::onResourceLoaderTestModules';

Hooks.php
@@ -435,4 +435,13 @@ class Hooks {
 		$returnToQuery = $returnToQuery . '&gettingStartedReturn=true';
 		return true;
 	}
+
+	public static function onResourceLoaderTestModules( array &$testModules, \ResourceLoader &$resourceLoader ) {
+		$testModules[ 'qunit' ][ 'ext.gettingstarted.user.tests' ] = array(
+			'scripts' => array( 'tests/qunit/ext.gettingstarted.user.test.js' ),
+			'dependencies' => array( 'ext.gettingstarted.user' ),
+			'localBasePath' => __DIR__,
+			'remoteExtPath' => 'GettingStarted',
+		);
+	}
 }

resources/ext.gettingstarted.user.js
@@ -1,0 +1,74 @@
+( function ( mw, $ ) {
+
+	'use strict';
+
+	var user,
+		tokenCookieName = mw.config.get( 'wgCookiePrefix' ) + 'gettingStartedUserToken';
+
+	mw.gettingStarted = mw.gettingStarted || {};
+
+	/**
+	 * The GettingStarted user API.
+	 *
+	 * This is currently used as part of the [Signup CTA experiment][0], which is
+	 * part of the [Anonymous editor acquisition][1] research project.
+	 *
+	 * [0]: https://meta.wikimedia.org/wiki/Research:Anonymous_editor_acquisition/Signup_CTA_experiment
+	 * [1]: https://meta.wikimedia.org/wiki/Research:Anonymous_editor_acquisition
+	 *
+	 * @class mw.gettingStarted.user
+	 * @singleton
+	 */
+	mw.gettingStarted.user = user = {
+
+		/**
+		 * Gets the user's token from the "gettingStartedUserToken"
+		 * cookie. If the cookie isn't set, then a token is generated,
+		 * stored in the cookie for 90 days, and then returned.
+		 *
+		 * See `mw.user.generateRandomSessionId`.
+		 *
+		 * @return {string}
+		 */
+		getToken: function () {
+			var storedToken = $.cookie( tokenCookieName ),
+				generatedToken;
+
+			if ( storedToken ) {
+				return storedToken;
+			}
+
+			generatedToken = mw.user.generateRandomSessionId();
+
+			$.cookie( tokenCookieName, generatedToken, {
+				expires: 90, // (days)
+				path: '/'
+			} );
+
+			return generatedToken;
+		},
+
+		/**
+		 * Gets the bucket that the user is in based on their token.
+		 *
+		 * Currently, the bucket corresponds to the variant of the
+		 * Signup CTA experiement that the user will be shown: pre-edit,
+		 * post-edit, or control.
+		 *
+		 * @return {string}
+		 */
+		getBucket: function () {
+			var token = user.getToken(),
+				lastCharacter = token.slice( -1 );
+
+			if ( lastCharacter <= 'J' ) {
+				return 'pre-edit';
+			} else if ( lastCharacter > 'J' && lastCharacter <= 'd' ) {
+				return 'post-edit';
+			} else {
+				return 'control';
+			}
+		}
+	};
+
+} ( mediaWiki, jQuery ) );

tests/qunit/ext.gettingstarted.user.test.js
@@ -1,0 +1,77 @@
+( function ( QUnit, mw, $ ) {
+
+	'use strict';
+
+	var expectedTokenCookieName = mw.config.get( 'wgCookiePrefix' ) + 'gettingStartedUserToken';
+
+	QUnit.module( 'ext.gettingstarted.user.getToken', {
+		setup: function () {
+			this.stub( mw.user, 'generateRandomSessionId' );
+			mw.user.generateRandomSessionId.returns( '1234' );
+
+			this.stub( $, 'cookie' );
+			$.cookie.returns( null );
+		}
+	} );
+
+	QUnit.test( 'should try to get the "token" cookie', 1, function ( assert ) {
+		mw.gettingStarted.user.getToken();
+
+		assert.strictEqual( $.cookie.lastCall.args[ 0 ], expectedTokenCookieName );
+	} );
+
+	QUnit.test( 'should use the token from the "token" cookie when it is set', 1, function ( assert ) {
+		$.cookie.returns( '5678' );
+
+		assert.strictEqual( mw.gettingStarted.user.getToken(), '5678' );
+	} );
+
+	QUnit.test( 'should generate the token when the "token" cookie isn\'t set', 1, function ( assert ) {
+		assert.strictEqual( mw.gettingStarted.user.getToken(), '1234' );
+	} );
+
+	QUnit.test( 'should store the generated token when the cookie isn\'t set', 1, function ( assert ) {
+		mw.gettingStarted.user.getToken();
+
+		assert.deepEqual( $.cookie.lastCall.args, [
+			expectedTokenCookieName,
+			'1234',
+			{
+				expires: 90,
+				path: '/'
+			}
+		] );
+	} );
+
+	QUnit.module( 'ext.gettingstarted.user.getBucket', {
+		setup: function () {
+			this.stub( mw.gettingStarted.user, 'getToken' );
+		}
+	} );
+
+	$.each( [
+		{
+			characters: '0123456789ABCDEFGHIJ',
+			expectedBucket: 'pre-edit'
+		},
+		{
+			characters: 'KLMNOPQRSTUVWXYZabcd',
+			expectedBucket: 'post-edit'
+		},
+		{
+			characters: 'efghijklmnopqrstuvwxyz',
+			expectedBucket: 'control'
+		}
+	], function ( i, data ) {
+		QUnit.test( data.characters, data.characters.length, function ( assert ) {
+			var j;
+
+			for ( j = 0; j < data.characters.length; ++j ) {
+				mw.gettingStarted.user.getToken.returns( data.characters[ j ] );
+
+				assert.strictEqual( mw.gettingStarted.user.getBucket(), data.expectedBucket);
+			}
+		} );
+	} );
+
+} ( QUnit, mediaWiki, jQuery ) );

commit 5387b28d24bff938c42812b829944ecb7f89c833

Module for user tokening and bucketing
* Create the mw.gettingStarted.user object, which can be used to get a
  user's token and the bucket that they are in (getToken and getBucket
  respectively)
* Make mw.gettingStarted.user.getToken transparently persist the token
  to a cookie for 90 days

Based on prior commit, 533c7f3b5821f6637f349dd0fd8ecd6a0de660d0, which
caused a Varnish problem due to having Token in the cookie name.

This replaces it with Id, and adds a - so the prefix doesn't run into
into the name.  It still has the exact string 'gettingStarted', so the
Varnish workaround at Ice1648d1c240af087992f31781887f561f55b288 will
apply.

See https://meta.wikimedia.org/wiki/Research:Anonymous_editor_acquisition/Signup_CTA_experiment

Change-Id: I34ed8bc0f8143dac981114e3cd125511ac66537a

GettingStarted.php
@@ -245,6 +245,14 @@ $wgResourceModules[ 'ext.gettingstarted.return' ] = array(
 	'position' => 'top',
 ) + $gettingStartedModuleInfo;
 
+$wgResourceModules[ 'ext.gettingstarted.user' ] = array(
+	'scripts' => 'ext.gettingstarted.user.js',
+	'dependencies' => array(
+		'mediawiki.user',
+		'jquery.cookie',
+	),
+) + $gettingStartedModuleInfo;
+
 $wgDefaultUserOptions[ GettingStarted\Hooks::INTRO_OPTION ] = true;
 
 $wgHooks[ 'BeforePageDisplay' ][] = 'GettingStarted\Hooks::onBeforePageDisplay';
@@ -259,3 +267,4 @@ $wgHooks[ 'GetPreferences' ][] = 'GettingStarted\Hooks::onGetPreferences';
 $wgHooks[ 'UserLogoutComplete'][] = 'GettingStarted\Hooks::onUserLogoutComplete';
 // Extension:CentralAuth's hook
 $wgHooks[ 'CentralAuthPostLoginRedirect' ][] = 'GettingStarted\Hooks::onCentralAuthPostLoginRedirect';
+$wgHooks[ 'ResourceLoaderTestModules' ][] = 'GettingStarted\Hooks::onResourceLoaderTestModules';

Hooks.php
@@ -435,4 +435,13 @@ class Hooks {
 		$returnToQuery = $returnToQuery . '&gettingStartedReturn=true';
 		return true;
 	}
+
+	public static function onResourceLoaderTestModules( array &$testModules, \ResourceLoader &$resourceLoader ) {
+		$testModules[ 'qunit' ][ 'ext.gettingstarted.user.tests' ] = array(
+			'scripts' => array( 'tests/qunit/ext.gettingstarted.user.test.js' ),
+			'dependencies' => array( 'ext.gettingstarted.user' ),
+			'localBasePath' => __DIR__,
+			'remoteExtPath' => 'GettingStarted',
+		);
+	}
 }

resources/ext.gettingstarted.user.js
@@ -1,0 +1,75 @@
+( function ( mw, $ ) {
+
+	'use strict';
+
+	var user,
+		// Cookie name can not have the word Token, to avoid serious Varnish caching problems.
+		tokenCookieName = mw.config.get( 'wgCookiePrefix' ) + '-gettingStartedUserId';
+
+	mw.gettingStarted = mw.gettingStarted || {};
+
+	/**
+	 * The GettingStarted user API.
+	 *
+	 * This is currently used as part of the [Signup CTA experiment][0], which is
+	 * part of the [Anonymous editor acquisition][1] research project.
+	 *
+	 * [0]: https://meta.wikimedia.org/wiki/Research:Anonymous_editor_acquisition/Signup_CTA_experiment
+	 * [1]: https://meta.wikimedia.org/wiki/Research:Anonymous_editor_acquisition
+	 *
+	 * @class mw.gettingStarted.user
+	 * @singleton
+	 */
+	mw.gettingStarted.user = user = {
+
+		/**
+		 * Gets the user's token from the "-gettingStartedUserId"
+		 * cookie. If the cookie isn't set, then a token is generated,
+		 * stored in the cookie for 90 days, and then returned.
+		 *
+		 * See `mw.user.generateRandomSessionId`.
+		 *
+		 * @return {string}
+		 */
+		getToken: function () {
+			var storedToken = $.cookie( tokenCookieName ),
+				generatedToken;
+
+			if ( storedToken ) {
+				return storedToken;
+			}
+
+			generatedToken = mw.user.generateRandomSessionId();
+
+			$.cookie( tokenCookieName, generatedToken, {
+				expires: 90, // (days)
+				path: '/'
+			} );
+
+			return generatedToken;
+		},
+
+		/**
+		 * Gets the bucket that the user is in based on their token.
+		 *
+		 * Currently, the bucket corresponds to the variant of the
+		 * Signup CTA experiement that the user will be shown: pre-edit,
+		 * post-edit, or control.
+		 *
+		 * @return {string}
+		 */
+		getBucket: function () {
+			var token = user.getToken(),
+				lastCharacter = token.slice( -1 );
+
+			if ( lastCharacter <= 'J' ) {
+				return 'pre-edit';
+			} else if ( lastCharacter > 'J' && lastCharacter <= 'd' ) {
+				return 'post-edit';
+			} else {
+				return 'control';
+			}
+		}
+	};
+
+} ( mediaWiki, jQuery ) );

tests/qunit/ext.gettingstarted.user.test.js
@@ -1,0 +1,77 @@
+( function ( QUnit, mw, $ ) {
+
+	'use strict';
+
+	var expectedTokenCookieName = mw.config.get( 'wgCookiePrefix' ) + '-gettingStartedUserId';
+
+	QUnit.module( 'ext.gettingstarted.user.getToken', {
+		setup: function () {
+			this.stub( mw.user, 'generateRandomSessionId' );
+			mw.user.generateRandomSessionId.returns( '1234' );
+
+			this.stub( $, 'cookie' );
+			$.cookie.returns( null );
+		}
+	} );
+
+	QUnit.test( 'should try to get the "token" cookie', 1, function ( assert ) {
+		mw.gettingStarted.user.getToken();
+
+		assert.strictEqual( $.cookie.lastCall.args[ 0 ], expectedTokenCookieName );
+	} );
+
+	QUnit.test( 'should use the token from the "token" cookie when it is set', 1, function ( assert ) {
+		$.cookie.returns( '5678' );
+
+		assert.strictEqual( mw.gettingStarted.user.getToken(), '5678' );
+	} );
+
+	QUnit.test( 'should generate the token when the "token" cookie isn\'t set', 1, function ( assert ) {
+		assert.strictEqual( mw.gettingStarted.user.getToken(), '1234' );
+	} );
+
+	QUnit.test( 'should store the generated token when the cookie isn\'t set', 1, function ( assert ) {
+		mw.gettingStarted.user.getToken();
+
+		assert.deepEqual( $.cookie.lastCall.args, [
+			expectedTokenCookieName,
+			'1234',
+			{
+				expires: 90,
+				path: '/'
+			}
+		] );
+	} );
+
+	QUnit.module( 'ext.gettingstarted.user.getBucket', {
+		setup: function () {
+			this.stub( mw.gettingStarted.user, 'getToken' );
+		}
+	} );
+
+	$.each( [
+		{
+			characters: '0123456789ABCDEFGHIJ',
+			expectedBucket: 'pre-edit'
+		},
+		{
+			characters: 'KLMNOPQRSTUVWXYZabcd',
+			expectedBucket: 'post-edit'
+		},
+		{
+			characters: 'efghijklmnopqrstuvwxyz',
+			expectedBucket: 'control'
+		}
+	], function ( i, data ) {
+		QUnit.test( data.characters, data.characters.length, function ( assert ) {
+			var j;
+
+			for ( j = 0; j < data.characters.length; ++j ) {
+				mw.gettingStarted.user.getToken.returns( data.characters[ j ] );
+
+				assert.strictEqual( mw.gettingStarted.user.getBucket(), data.expectedBucket);
+			}
+		} );
+	} );
+
+} ( QUnit, mediaWiki, jQuery ) );