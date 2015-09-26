95ede3055a361a1a5327fb6792eed1102df19b36
Željko Filipin <zeljko.filipin@gmail.com>
2015-09-26 17:39:06 +0200

    Fixed Style/Tab RuboCop offense
    As a side effects, Style/IndentationWidth offense is also fixed.
    
    Bug: T113846
    Change-Id: I2dc9d48470bba35bba64415a5d1fa115418aaf01

--- tests/browser/features/step_definitions/modal_on_editable_returnto_steps.rb
+++ tests/browser/features/step_definitions/modal_on_editable_returnto_steps.rb
@@ -1,26 +1,26 @@
 When(/^I register$/) do
-	@original_title = on(EditableRandomPage).title
+  @original_title = on(EditableRandomPage).title
 
-	on(EditableRandomPage).create_account_element.click
-	on(RegistrationPage).register_user "Test Account #{@random_string}"
+  on(EditableRandomPage).create_account_element.click
+  on(RegistrationPage).register_user "Test Account #{@random_string}"
 end
 
 Then(/^I should be returned to the same article$/) do
-	expect(on(EditableRandomPage).title).to eq @original_title
+  expect(on(EditableRandomPage).title).to eq @original_title
 end
 
 Then(/^I should see a modal call to action$/) do
-	expect(on(EditableRandomPage).cta_element.when_present).to be_visible
+  expect(on(EditableRandomPage).cta_element.when_present).to be_visible
 end
 
 Then(/^one action is edit this page$/) do
-	expect(on(EditableRandomPage).edit_this_page_element).to be_visible
+  expect(on(EditableRandomPage).edit_this_page_element).to be_visible
 end
 
 Then(/^one action is edit a suggested page$/) do
-	expect(on(EditableRandomPage).edit_suggested_page_element).to be_visible
+  expect(on(EditableRandomPage).edit_suggested_page_element).to be_visible
 end
 
 Then(/^one action is no thanks, maybe later$/) do
-	expect(on(EditableRandomPage).no_thanks_element).to be_visible
+  expect(on(EditableRandomPage).no_thanks_element).to be_visible
 end
