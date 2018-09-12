23d3f57c6b9bcfa240695e7c5796dad2a34a4c8b
Fomafix <fomafix@googlemail.com>
2018-09-12 06:37:08 +0200

    Start showing warning for deprecated PHP entry point
    Also update the required MediaWiki version to required version in
    extension.json.
    
    Change-Id: I345789e88e0ba98d032aa6de73bde679648ac9e3

--- GettingStarted.php
+++ GettingStarted.php
@@ -3,12 +3,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'GettingStarted' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['GettingStarted'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for GettingStarted extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the GettingStarted extension requires MediaWiki 1.25+' );
+	die( 'This version of the GettingStarted extension requires MediaWiki 1.29+' );
 }
