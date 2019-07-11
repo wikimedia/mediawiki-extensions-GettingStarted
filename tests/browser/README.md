5fdc18e8a422ffbaf45609c0315fd45e8af99010
Željko Filipin <zeljko.filipin@gmail.com>
2019-07-11 14:01:53 +0200

    Delete Ruby Selenium tests
    Ruby Selenium framework has been deprecated since 2017. We are removing
    support for it from CI.
    Tests are not providing any value. They are not triggered by Zuul.
    Jenkins job that runs them daily has been broken for months.
    
    Bug: T180224
    Bug: T214382
    Bug: T220035
    Depends-On: Iec6c5ec6c1ea1a30b8ea2bd51188d4a87b0aeb0a
    Change-Id: Icc7bee6b4c5b55d71b2ec322f1e5012a3a9664e6

--- tests/browser/README.md
+++
@@ -1,5 +0,0 @@
-# Browser tests
-
-Run the tests with `bundle exec cucumber`.
-
-See <https://phabricator.wikimedia.org/diffusion/MSEL/browse/master/README.md> for instructions on how to configure your development environment for running the tests.
--- tests/browser/features/support/pages/editable_random_page.rb
+++
@@ -1,8 +0,0 @@
-class EditableRandomPage < RandomPage
-  h1(:title, id: 'firstHeading')
-  div(:cta, id: 'mw-gettingstarted-cta-editable-main-page')
-  a(:edit_this_page, id: 'mw-gettingstarted-editable-main-edit-page')
-  a(:edit_suggested_page, id: 'mw-gettingstarted-editable-main-fix-pages')
-  a(:no_thanks, class: 'mw-gettingstarted-cta-leave-link')
-  a(:create_account, css: '#pt-createaccount a')
-end
