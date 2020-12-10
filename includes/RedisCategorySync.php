8334730467b0978f4b796a42d8caf14001bd5a77
Aaron Schulz <aschulz@wikimedia.org>
2020-12-09 16:57:59 -0800

    Avoid calls to deprecated Database::onTransactionIdle() method
    Change-Id: Ia47bab36e4b2738f5b4c70e51015a9b7c5b7f22f

--- includes/RedisCategorySync.php
+++ includes/RedisCategorySync.php
@@ -188,7 +188,7 @@ class RedisCategorySync {
 		self::$callbackSet = true;
 
 		$dbw = wfGetDB( DB_MASTER );
-		$dbw->onTransactionIdle( function () {
+		$dbw->onTransactionCommitOrIdle( function () {
 			// Any category updates that happen after this will require an
 			// additional run of the callback.
 			RedisCategorySync::$callbackSet = false;
